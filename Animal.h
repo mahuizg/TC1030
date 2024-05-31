9184d305d1c8edb24e04b18f205b32756503eccc
mahuizg <150102911+mahuizg@users.noreply.github.com>
2024-05-31 00:05:32 -0600

    Add files via upload
    Actualización de los archivos para el avance 3

---
+++ Animal.h
@@ -0,0 +1,74 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+protected: //Al ser la clase Animal la clase madre para Perro y Gato, sus atributos deben ser protegidos, de manera que las clases mencionadas anteriormente tengan acceso a dichos atributos. 
+    int id;
+    string tipo;
+    string nombre;
+    int edad;
+    char sexo;
+    string raza;
+    string color;
+
+public:
+    Animal(int id_, string tipo_, string nombre_, int edad_, char sexo_, string raza_, string color_); //Declaro el constructor del objeto tipo Animal y que contiene todos los atributos que sí son compartidos por perros y gatos
+    int getId() { return id; }
+    string getTipo() { return tipo; }
+    string getNombre() { return nombre; }
+    int getEdad() { return edad; }
+    char getSexo() { return sexo; }
+    string getRaza() { return raza; }
+    string getColor() { return color; }
+    virtual ~Animal() {} //Esta línea define un destructor virtual, de manera que se destruyan los objetos de tipo Animal (y aquellos creados mediante la herencia que Animal tiene sobre Perro y Gato). 
+};
+
+Animal::Animal(int id_, string tipo_, string nombre_, int edad_, char sexo_, string raza_, string color_){
+    id = id_;
+    tipo = tipo_;
+    nombre = nombre_;
+    edad = edad_;
+    sexo = sexo_;
+    raza = raza_;
+    color = color_;
+}
+
+class Perro : public Animal { //Creo la clase Perro, la cual tiene una relación de herencia con la clase madre Animal
+private: //Debido a que estos atributos no deberán ser heredados por ninguna otra clase, serán declarados como privados.
+    string tamanio;
+    bool entrenado;
+    string entrenador;
+
+public:
+    //En los dos siguientes métodos, veremos dos constructores que lucen casi idénticos, pero el primero será utilizado para los perros que no estén entrenados. De ahí viene que la variable entrenado sea false
+    Perro(int id_, string tipo_, string nombre_, int edad_, char sexo_, string raza_, string color_, string tamanio_, bool entrenado_) : Animal(id_, tipo_, nombre_, edad_, sexo_, raza_, color_){
+        tamanio = tamanio_;
+        entrenado = false;
+    }
+    //El siguiente constructor nos ayudará a crear objetos de tipo Perro pero que sí estén entrenados. Debido a esto, entrenado será true, y también usaremos otro atributo llamado "entrenador", el cual no tendría sentido que usara si llegara a crear perros no entrenados.
+    //La intención al tener dos distintos constructores para perro, es demostrar la competencia que involucra el uso de sobrecarga. 
+    Perro(int id_, string tipo_, string nombre_, int edad_, char sexo_, string raza_, string color_, string tamanio_, bool entrenado_, string entrenador_) : Animal(id_, tipo_, nombre_, edad_, sexo_, raza_, color_){
+        tamanio = tamanio_;
+        entrenado = true;
+        entrenador = entrenador_;
+    }
+    bool getEntrenado() { return entrenado; }
+    string getEntrenador() { return entrenador; }
+};
+
+class Gato : public Animal {
+private: //Declaro los atributos que pertenecen únicamente a los objetos de tipo Gato. 
+    int independencia;
+
+public:
+    Gato(int id_, string tipo_, string nombre_, int edad_, char sexo_, string raza_, string color_, int independencia_) : Animal(id_, tipo_, nombre_, edad_, sexo_, raza_, color_){ //Constructor de los objetos de tipo Gato. 
+        independencia = independencia_;
+    }
+    int getIndependencia() { return independencia; }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,125 @@
+//TEMAS A RESOLVER: 
+// 1 - Cuando imprimo los animales, donde dice "Entrenado: " ahora regresa un 1 esté el perro entrenado o no
+// 2 - ¿Por qué se está imprimiendo "Entrenador: " incluso cuando el perro no está etrenado. Creo que esto tiene que ver con el error 1. 
+// 3 - Tengo que seguir haciendo modificaciones en registrarAnimal()
+// 4 - Si bien sí se elimina el animal que se encuentre en el índice que indique, también se dejan de imprimir todos los animales cuyo índice este después del índice del animal eliminado. 
+
+#include "Refugio.h"
+#include "Animal.h"
+#include <iostream>
+
+using namespace std;
+
+Refugio refugio;
+
+void initAnimales(){ //En las siguientes líneas, crearé mis objetos. A su vez, usaré la funcion .registrarAnimal() para poder ir ingresando cada uno de estos objetos al arreglo que contendrá todos los animale. 
+    refugio.registrarAnimal(1, "Perro", "Miguel Bosé", 3, 'M', "Pug", "Beige", "Chico", true, "Jaime", 0);
+    refugio.registrarAnimal(2, "Perro", "Pepa", 10, 'H', "Beagle", "Blanco con café", "Mediano", false, "", 0);
+    refugio.registrarAnimal(3, "Perro", "Raúl", 9, 'M', "Airedale Terrier", "Café con negro", "Grande", true, "Jimena", 0);
+    refugio.registrarAnimal(4, "Gato", "Bernarda", 2, 'H', "Bombay", "Negro", "", false, "", 9);
+    refugio.registrarAnimal(5, "Gato", "Pate", 7, 'M', "Siberiano", "Gris", "", false, "", 4);
+} //Como podemos ver, aquellos atributos que no aplican para cierto animal, simplemente los dejé vacíos, pero no los dejé fuera del cosntructor
+
+int menu(){
+    int opcion;
+    bool continua; //Establezco una variable booleana con un valor inciial de true para que el menu se despliegue una y otra vez mientras el usuario no escoga la opción para salir.
+    continua = true;
+    while (continua == true){ //Las siguientes líneas imprimen los textos necesarios para mi menú inicial
+        cout << endl;
+        cout << "Bienvenido" << endl;
+        cout << "1. Ver animales" << endl;
+        cout << "2. Agregar animal" << endl;
+        cout << "3. Eliminar animal" << endl;
+        cout << "4. Salir" << endl;
+        cout << endl;
+        cout << "Selecciona una opción: " << endl;
+        cin >> opcion; //Solicito una entrada al usuario para poder indicarle al código qué funciones ejecutar.
+        if (opcion == 1){ // Si el usuario desea ver los animales, llamaré el método mostrarAnimales() el cual pertenece a la clase Refugio, y cuya lógica see encuentra en el archivo Refugio.h
+            refugio.mostrarAnimales();
+        }
+
+        else if (opcion == 2){ 
+            cout << "¿Cuál es el número de ID del animal que quieres dar de alta?: " << endl;
+            int id; 
+            cin >> id;
+            cout << endl; 
+            cout << "¿Qué tipo de animal es? Escribe el tipo de animal con la primera letra mayúscula únicamente y sin espacios al final: ";
+            string tipo; 
+            cin >> tipo; 
+            cout << endl; 
+            cout << "¿Cuál es el nombre del animal?: " << endl; 
+            string nombre; 
+            cin >> nombre;
+            cout << endl; 
+            cout << "¿Cuántos años tiene el animal?: " << endl; 
+            int edad; 
+            cin >> edad; 
+            cout << endl; 
+            cout << "¿Cuál es su sexo? Asegúrate de ingresar únicamente un caracter (H para hembra, M para macho): " << endl;
+            char sexo; 
+            cin >> sexo; 
+            cout << endl; 
+            cout << "¿Cuál es la raza del animal?: " << endl;
+            string raza; 
+            cin >> raza; 
+            cout << endl;
+            cout << "¿Cuál es el color del animal?: " << endl; 
+            string color; 
+            cin >> color; 
+            cout << endl;
+            cout << "Si el animal es un gato, simplemente ingresa NA" << endl; 
+            cout << "Si el animal es un perro, ¿su tamaño es chico, mediano, o grande? Recuerda ingresar tu respuesta en una sola palabra sin espacios y con la primera letra en mayúsculas: "<< endl;
+            string tamanio; 
+            cin >> tamanio; 
+            cout << endl;
+            cout << "Si el animal es un gato, ingrese 0" << endl; 
+            cout << "Si el animal es un perro, ¿está entrenado? Escribe 0 si no lo está, o 1 en el caso contrario: " << endl; 
+            int entrada; 
+            cin >> entrada;
+            bool entrenado;
+            if (entrada == 0){
+                entrenado = false;
+            }
+            else if (entrada == 1){
+                entrenado = true;
+            }
+            cout << endl; 
+            cout << "Si el animal es un gato, ingrese NA" << endl;
+            cout << "Si el animal es un perro que no esté entrenado, ingresa NA. Si el perro sí está entrenado, ¿cuál es el nombre de su entrenador/a?: " << endl;
+            string entrenador;
+            cin >> entrenador; 
+            cout << endl; 
+            cout << "Si el animal es un perro, ingresa 0" << endl; 
+            cout << "Si el animal es un gato, ¿cuál es su nivel de independencia en una escala del 1-10?: " << endl; 
+            int independencia; 
+            cin >> independencia; 
+
+            refugio.registrarAnimal(id, tipo, nombre, edad, sexo, raza, color, tamanio, entrenado, entrenador, independencia);
+        }
+
+        else if (opcion == 3){
+            cout << "¿Cuál es el id del animal que quieres eliminar?: "<< endl; 
+            int indice;
+            cin >> indice; 
+            refugio.eliminarAnimal(indice);
+        }
+
+        else if (opcion == 4){ //Esta condicional se despide del usuario y finalmente rompe el ciclo que hacía que el menú se desplegará hasta qeu el usuario solicitara salir. 
+            cout << endl;
+            cout << "Hasta luego" << endl;
+            cout << endl;
+            continua = false; //Establecemos la variable continua como falsa para salir del ciclo
+        }
+
+        else{ //Esta última condición la escribí en caso de que el usuario escriba cualquier otra opción que no esté contenida en el menú. 
+            cout << "Por favor, selecciona una opción válida" << endl;
+        }
+    }
+    return 0;
+}
+
+int main(){ //Dentro de mi función main, llamo a la función que me ayudará a inicializar todos los objetos de tipo Animal. De igual manera, llamo a la función que desplega el menú. 
+    initAnimales();
+    menu();
+    return 0;
+}
